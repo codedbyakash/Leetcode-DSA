1b8079b65bffbae5310f6f07882b678f8e7df486
Akash yadav <ay9514221@gmail.com>
2026-06-11 16:05:15 +0530

    Time: 3 ms (37.8%), Space: 40.3 MB (43.71%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -26,9 +26,31 @@ public:
 
         //  methode --2:
 
+        // int n=nums.size();
+        // vector<int>prefix(n);
+        // vector<int>suffix(n);
+        // //  prefix product 
+        // int p=nums[0];
+        // prefix[0]=1;
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=p;
+        //     p*=nums[i];
+        // }
+        // p=nums[n-1];
+        // suffix[n-1]=1;
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=p;
+        //     p*=nums[i];
+        // }
+        // for(int i=0;i<n;i++){
+        //     prefix[i]*=suffix[i];
+        // }
+
+       
+       // methode -3
+
         int n=nums.size();
         vector<int>prefix(n);
-        vector<int>suffix(n);
         //  prefix product 
         int p=nums[0];
         prefix[0]=1;
@@ -36,17 +58,12 @@ public:
             prefix[i]=p;
             p*=nums[i];
         }
-        p=nums[n-1];
-        suffix[n-1]=1;
+        p=1;
+        // prefix[n-1]=prefix[n-1]*1;
         for(int i=n-2;i>=0;i--){
-            suffix[i]=p;
-            p*=nums[i];
+            p*=nums[i+1];
+            prefix[i]*=p;
         }
-        for(int i=0;i<n;i++){
-            prefix[i]*=suffix[i];
-        }
-
-       
     return prefix;
     }
     
