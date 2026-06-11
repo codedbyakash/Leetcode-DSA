7074820a193e1364103d56b6b8aa93f25666653e
Akash yadav <ay9514221@gmail.com>
2026-06-11 15:52:08 +0530

    Time: 4 ms (20.87%), Space: 41.3 MB (39.31%) - LeetHub

--- 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
+++ 0238-product-of-array-except-self/0238-product-of-array-except-self.cpp
@@ -1,6 +1,31 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        //  methode -1:
+
+        // int n=nums.size();
+        // vector<int>prefix(n);
+        // vector<int>suffix(n);
+        // //  prefix product 
+        // int p=nums[0];
+        // prefix[0]=1;
+        // for(int i=1;i<n;i++){
+        //     prefix[i]=p;
+        //     p*=nums[i];
+        // }
+        // p=nums[n-1];
+        // suffix[n-1]=1;
+        // for(int i=n-2;i>=0;i--){
+        //     suffix[i]=p;
+        //     p*=nums[i];
+        // }
+        // vector<int >ans(n);
+        // for(int i=0;i<n;i++){
+        //     ans[i]=prefix[i]*suffix[i];
+        // }
+
+        //  methode --2:
+
         int n=nums.size();
         vector<int>prefix(n);
         vector<int>suffix(n);
@@ -17,36 +42,12 @@ public:
             suffix[i]=p;
             p*=nums[i];
         }
-        vector<int >ans(n);
         for(int i=0;i<n;i++){
-            ans[i]=prefix[i]*suffix[i];
+            prefix[i]*=suffix[i];
         }
-        // int p=1;
-        // prefix[0]=1;
-        // for(int i=1;i<n;i++){
-        //     p*=nums[i];
-        //     prefix[i]=p;
-        // }
-        // p=1;
-        // prefix[n-2]=prefix[n-2]*p;
-        // for(int i=n-1;i>=0;i--){
-        //     prefix[i]*=p;
-        //     p*=nums[i];
-        // }
-        //  suffix product 
-    //     vector<int>suffix;
-    //     for(int i=n-1;i>=0;i--){
-    //         if(i==n-1){
-    //             suffix[i]=p;
-    //             p=nums[i];
-    //         }
-    //         suffix[i]=p;
-    //         p*=nums[i];
-    //     }
-    //     for(int i=0;i<n;i++){
-    //         prefix[i]=prefix[i]*suffix[i];
-    //     }
-    return ans;
+
+       
+    return prefix;
     }
     
 };
